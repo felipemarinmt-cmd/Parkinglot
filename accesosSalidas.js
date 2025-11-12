0348a13326524a1cefa8b8a9740434479fd3a3a5
Jose Bautista <121981137+josebautiista@users.noreply.github.com>
2025-11-12 16:29:13 -0500

    refactor: standardize code formatting and improve readability in accesosSalidas.js

--- accesosSalidas.js
+++ accesosSalidas.js
@@ -1,50 +1,54 @@
-const mongoose = require('mongoose'); // Import mongoose
+const mongoose = require("mongoose"); // Import mongooseDB
 
 // Schema for counter functionality
-const counterSchema = new mongoose.Schema({ // Schema for counter
-    _id: String,
-    seq: Number
+const counterSchema = new mongoose.Schema({
+  // Schema for counter
+  _id: String,
+  seq: Number,
 });
 
-const Counter = mongoose.models.Counter || mongoose.model('Counter', counterSchema); 
+const Counter =
+  mongoose.models.Counter || mongoose.model("Counter", counterSchema);
 
 // Main schema for AccesoSalida
-const accesoSalidaSchema = new mongoose.Schema({ 
-    idint: Number, //
-    cod_acceso: {
-        type: Number,
-        unique: true
-    },
-    cod_salida: {
-        type: Number,
-        required: true
-    },
-    nombre: {
-        type: String,
-        required: true,
-        trim: true
-    }
+const accesoSalidaSchema = new mongoose.Schema({
+  idint: Number, //
+  cod_acceso: {
+    type: Number,
+    unique: true,
+  },
+  cod_salida: {
+    type: Number,
+    required: true,
+  },
+  nombre: {
+    type: String,
+    required: true,
+    trim: true,
+  },
 });
 
 // Pre-save hook to auto-increment idint
-accesoSalidaSchema.pre('save', async function(next) { 
-    if (this.isNew) {
-        try {
-            const counter = await Counter.findByIdAndUpdate(
-                'idint_acceso_salida',
-                { $inc: { seq: 1 } },
-                { new: true, upsert: true }
-            );
-            this.idint = counter.seq;
-            next();
-        } catch (error) {
-            next(error);
-        }
-    } else {
-        next();
+accesoSalidaSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    try {
+      const counter = await Counter.findByIdAndUpdate(
+        "idint_acceso_salida",
+        { $inc: { seq: 1 } },
+        { new: true, upsert: true }
+      );
+      this.idint = counter.seq;
+      next();
+    } catch (error) {
+      next(error);
     }
+  } else {
+    next();
+  }
 });
 
-const AccesoSalida = mongoose.models.AccesoSalida || mongoose.model('AccesoSalida', accesoSalidaSchema);
+const AccesoSalida =
+  mongoose.models.AccesoSalida ||
+  mongoose.model("AccesoSalida", accesoSalidaSchema);
 
 module.exports = AccesoSalida;
