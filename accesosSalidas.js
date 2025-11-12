b9e5706386563f54f05412cf28fe015e472e5169
Jose Bautista <121981137+josebautiista@users.noreply.github.com>
2025-11-12 16:41:42 -0500

    fix: update comment for clarity in counter schema definition

--- accesosSalidas.js
+++ accesosSalidas.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose"); // Import mongooseDB
 
-// Schema for counter functionality
+// Schema for counter functionality plus
 const counterSchema = new mongoose.Schema({
   // Schema for counter
   _id: String,
