916f85fe1d2bcbe56c0e9552630d522bf6f67ec9
felipemarinmt-cdm <felipemarinmt@gmail.com>
2025-11-12 16:40:48 -0500

    refactor: standardize quotes and improve code formatting in incidencia.js

--- incidencia.js
+++ incidencia.js
@@ -1,29 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const counterSchema = new mongoose.Schema({
   _id: { type: String, required: true },
-  seq: { type: Number, default: 0 }
+  seq: { type: Number, default: 0 },
 });
 
-const Counter = mongoose.models.Counter || mongoose.model('Counter', counterSchema);
+const Counter =
+  mongoose.models.Counter || mongoose.model("Counter", counterSchema);
 
 const incidenciaSchema = new mongoose.Schema({
   idint: {
     type: Number,
-    unique: true
+    unique: true,
   },
   nombre: {
     type: String,
     required: true,
-    trim: true
-  }
+    trim: true,
+  },
 });
 
-incidenciaSchema.pre('save', async function(next) {
+incidenciaSchema.pre("save", async function (next) {
   if (this.isNew) {
     try {
       const counter = await Counter.findByIdAndUpdate(
-        'idint_incidencia',
+        "idint_incidencia",
         { $inc: { seq: 1 } },
         { new: true, upsert: true }
       );
@@ -35,5 +36,5 @@ incidenciaSchema.pre('save', async function(next) {
   next();
 });
 
-const Incidencia = mongoose.model('Incidencia', incidenciaSchema);
+const Incidencia = mongoose.model("Incidencia", incidenciaSchema);
 module.exports = Incidencia;
